f8157dcc405cc2d588f4ecc09edb9295f9256661
Dark Age Developers <calvin.baumgartner@darkage.io>
2021-06-30 02:33:05 +0000

    update to keep alive handler to process packets better

--- sc-server/keepalive_handler.py
+++ sc-server/keepalive_handler.py
@@ -1,30 +1,44 @@
 import socket
 import sys
 
-addr = ("", 8080)
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+def main():
 
-sock.bind(addr)
+    addr = ("", 8080)
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-#listen for incoming connections
-sock.listen(1)
-while True:
-    print("waiting for a connection")
-    connection, client_address = sock.accept()
+    sock.bind(addr)
 
-    try:
-        print("connection %s: %s" % (connection,client_address))
-        while True:
-            data = connection.recv(8)
-            print("received %s" % data)
+    #listen for incoming connections
+    sock.listen(1)
+    while True:
+        print("waiting for a connection")
+        connection, client_address = sock.accept()
 
-            if data:
-                print("sending response")
-                response_data = b'message received'
-                connection.sendall(response_data)
-            else:
-                print("no more data from client")
-                break
-    finally:
-        connection.close()
-        
+        try:
+            print("connection %s: %s" % (connection,client_address))
+            while True:
+                data = connection.recv(16)
+                print("received %s" % data)
+
+                if data:
+                    print("sending response")
+                    response_data = b'message received'
+                    connection.sendall(response_data)
+                    client_id = parse_client_keepalive(data)
+                    print("saw client id %d" % client_id)
+
+                else:
+                    print("no more data from client")
+                    break
+        finally:
+            connection.close()
+
+def parse_client_keepalive(client_pkt):
+    #clean up byte array by removing b''
+    client_pkt_clean = str(client_pkt).replace("b","").replace("'","")
+
+    client_id = int(client_pkt_clean.split(",")[0])
+    return client_id
+
+if __name__ == "__main__":
+    main()
