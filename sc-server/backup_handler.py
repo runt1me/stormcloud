b1980d0738c6f0389f58447ebc66907409fb193f
Dark Age Developers <calvin.baumgartner@darkage.io>
2021-07-22 01:59:30 +0000

    starts on handling file transfers from the server side

--- sc-server/backup_handler.py
+++ sc-server/backup_handler.py
@@ -17,14 +17,35 @@ def main():
         try:
             print("connection %s: %s" % (connection,client_address))
             while True:
-                data = connection.recv(560)
+                header = connection.recv(560)
                 current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-                if data:
-                    print(data)
+                if header:
+                    client_field = header[0:16]
+                    file_path_field = header[16:528]
+                    length_field = header[528:560]
+
+                    print('Receiving file from client %s' % client_field.decode('ascii'))
+                    print(file_path_field.decode('ascii'))
+
+                delimiter = connection.recv(11)
+                if delimiter:
+                    print(delimiter.decode('ascii'))
+
+                raw_content = connection.recv(get_content_length(length_field))
+                if raw_content:
+                    print("==RAW BYTES==")
+                    print(raw_content)
+                    print(len(raw_content))
 
         finally:
             connection.close()
+            exit()
+
+def get_content_length(length_field):
+    #replace null characters with '' for the purpose of converting string -> int
+    print("receiving %d bytes" % int(length_field.decode('ascii').replace('\x00','')))
+    return int(length_field.decode('ascii').replace('\x00',''))
 
 def parse_client_keepalive(client_pkt):
     #clean up byte array by removing b''
@@ -33,46 +54,8 @@ def parse_client_keepalive(client_pkt):
     client_id = int(client_pkt_clean.split(",")[0])
     return client_id
 
-def record_keepalive(client_id,current_time):
-    print("recording keepalive")
 
-    if not client_is_known(client_id):
-        add_client_to_file(client_id)
 
-    record_keepalive_for_client(client_id,current_time)
-
-def client_is_known(client_id):
-    clients = []
-    with open("/root/stormcloud/keepalives.csv","r") as keepalive_file:
-        for line in [l for l in keepalive_file.read().split("\n") if l]:
-            clients.append(int(line.split(",")[0]))
-
-    return client_id in clients
-
-def add_client_to_file(client_id):
-    with open("/root/stormcloud/keepalives.csv","a") as keepalive_file:
-        keepalive_file.write("%d,\n" % client_id)
-
-def record_keepalive_for_client(client_id,current_time):
-
-    #in order to update the file, read the whole thing first,
-    #find the line to modify and change it, and then rewrite the whole file
-    #this is a quick and dirty approach that wont apply once we implement a database
-    with open("/root/stormcloud/keepalives.csv","r") as keepalive_file:
-        original_lines = [l for l in keepalive_file.read().split("\n") if l]
-        clients = [int(l.split(",")[0]) for l in original_lines]
-        line_to_modify_idx = clients.index(client_id)
-
-        original_lines[line_to_modify_idx] = "%d,%s\n" % (client_id,current_time)
-
-    with open("/root/stormcloud/keepalives.csv","w") as keepalive_file:
-        for line in original_lines:
-            keepalive_file.write(line)
-
-def display_file():
-    with open("/root/stormcloud/keepalives.csv","r") as keepalive_file:
-        for line in [l for l in keepalive_file.read().split("\n") if l]:
-            print(line)
 
 if __name__ == "__main__":
     main()
