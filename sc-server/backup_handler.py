a8ffa392f355d127b6b2f231f931c609a27e724b
Dark Age Developers <rtchess.maniac@gmail.com>
2021-08-11 22:33:33 +0000

    adds basic logging to backup handler

--- sc-server/backup_handler.py
+++ sc-server/backup_handler.py
@@ -2,19 +2,18 @@ import socket
 import sys
 from datetime import datetime
 
+import logging
+
 def main():
-    addr = ("", 8083)
-    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-    sock.bind(addr)
-
+    initialize_logging()
+    sock = initialize_socket()
     #listen for incoming connections
     sock.listen(1)
     while True:
-        print("Listening for connections...")
+        logging.log(logging.INFO, "Listening for connections...")
         connection, client_address = sock.accept()
 
-        print("connection %s: %s" % (connection,client_address))
+        logging.log(logging.INFO, "connection %s: %s" % (connection,client_address))
         header = connection.recv(560)
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
@@ -23,14 +22,14 @@ def main():
             file_path = get_file_path(header[16:528])
             length = get_content_length(header[528:560])
 
-            print('Receiving file from client %d' % client)
-            print(file_path)
+            logging.log(logging.INFO,'Receiving file from client %d' % client)
+            logging.log(logging.INFO, file_path)
 
         check_result = perform_integrity_check_delimiter(connection.recv(11))
         if check_result:
-            print("delimiter validated, continuing")
+            logging.log(logging.INFO,"delimiter validated, continuing")
         else:
-            print("invalid delimiter")
+            logging.log(logging.INFO,"invalid delimiter")
 
         bytes_to_receive = length
         raw_content = b''
@@ -39,7 +38,7 @@ def main():
             bytes_recvd = connection.recv(bytes_to_receive)
             raw_content += bytes_recvd
             if raw_content:
-                print("(Received %d bytes)" % len(bytes_recvd))
+                logging.log(logging.INFO,"(Received %d bytes)" % len(bytes_recvd))
 
             bytes_to_receive = bytes_to_receive - len(bytes_recvd)
 
@@ -59,9 +58,9 @@ def get_client_id(client_id_field):
     return int(client_id_as_string)
 
 def store_file(client_id,file_path,file_length,file_raw_content):
-    print("== STORING FILE : %s ==" %file_path)
-    print("Client ID:\t%d" % client_id)
-    print("File Length:\t%d" % file_length)
+    logging.log(logging.INFO,"== STORING FILE : %s ==" %file_path)
+    logging.log(logging.INFO,"Client ID:\t%d" % client_id)
+    logging.log(logging.INFO,"File Length:\t%d" % file_length)
 
     #TODO: check and authenticate that its a legitimate client id
     #to prevent against spoofed packets and attacks
@@ -74,13 +73,28 @@ def store_file(client_id,file_path,file_length,file_raw_content):
     file_path_stripped = file_path.split("\\")[-1]
     path_on_server = client_id_root_folder + file_path_stripped
 
-    print("writing content to %s" % path_on_server)
+    logging.log(logging.INFO,"writing content to %s" % path_on_server)
     with open(path_on_server,'wb') as outfile:
         outfile.write(file_raw_content)
 
 def perform_integrity_check_delimiter(delim):
     return delim == "~||~TWT~||~"
 
+def initialize_logging():
+    logging.basicConfig(
+            filename='/var/log/stormcloud.log',
+            filemode='a',
+            format='%(asctime)s %(levelname)-8s %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S',
+            level=logging.DEBUG
+    )
+
+def initialize_socket():
+    addr = ("", 8083)
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    sock.bind(addr)
+    return sock
 
 if __name__ == "__main__":
     main()
