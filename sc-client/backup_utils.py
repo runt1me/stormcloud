c5da928a82420e8fad866777a6557db1daa00489
Ryan Thomas <rtchess.maniac@gmail.com>
2021-08-31 20:12:57 -0400

    refactor and add client logging send to server

--- sc-client/backup_utils.py
+++ sc-client/backup_utils.py
@@ -11,20 +11,13 @@ import socket
 import logging
 
 import crypto_utils
+import network_utils
 
 #connection port for file backup
 #TODO: maybe have the server use multiple ports for clients?
 #idea: clients could first reach out to the "request port" which would ask the server
 #for a port to communicate on for the transfers, and then it could go from there
 CONNECTION_PORT = 8083
-CONNECTION_SERVER = "www2.darkage.io"
-
-HEADER_PORTION_CLIENT_LEN = 16
-HEADER_PORTION_PATH_LEN   = 512
-HEADER_PORTION_SIZE_LEN   = 32
-
-#I love you, T :)
-DELIMITER="~||~TWT~||~"
 
 #return codes for checking hash db
 BACKUP_STATUS_NO_CHANGE = 0
@@ -83,72 +76,7 @@ def process_file(file_path_obj,client_id):
             file_content = file_path_obj.read_bytes()
             file_size = file_path_obj.stat().st_size
         
-            ship_file_to_server(client_id,file_path,file_content,file_size)
-
-def ship_file_to_server(client_id,path,content,size):
-    encrypted_content, encrypted_size = crypto_utils.encrypt_file(path)
-
-    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    server_address = (CONNECTION_SERVER,CONNECTION_PORT)
-
-    logging.log(logging.INFO,"connecting to %s port %s" % server_address)
-    sock.connect(server_address)
-    try:
-        dump_file_info(path,size,encrypted_size)
-        message = wrap_file_for_delivery(client_id,path,encrypted_content,encrypted_size)
-        sock.sendall(message)
-
-        #TODO: server response to client??
-        #maybe respond with hash as verification check?
-        #or is the security of tcp enough?
-
-    finally:
-        logging.log(logging.INFO,"closing socket")
-        sock.close()
-
-    sleep(3)
-
-def wrap_file_for_delivery(client_id,path,content,size):
-    # PACKET STRUCTURE
-    # +-----------------------------------------+
-    # | HEADER       = 560 bytes                |
-    # | DELIMITER    =  11 bytes                |
-    # | CONTENT      =   n bytes*               |
-    # | *content size defined                   |
-    # |  in size field of header (offset 528)   |
-    # +-----------------------------------------+
-    # content begins at offset 571
-    wrapped_header = wrap_header(client_id,path,size)
-    return wrapped_header + DELIMITER.encode('ascii') + content
-
-def wrap_header(client_id,path,size):
-    # HEADER PACKET BREAKDOWN
-    # +-----------------------------------------+
-    # | client_id    -> padded to 16 bytes      |
-    # | path         -> padded to 512 bytes     |
-    # | size of file -> padded to 32 bytes      |
-    # +-----------------------------------------+
-    # Total header size: 560 bytes
-    padded_client_id = pad_client_id(client_id)
-    padded_path      = pad_path(path)
-    padded_size      = pad_size(size)
-
-    return padded_client_id + padded_path + padded_size
-
-def pad_client_id(client_id):
-    len_to_pad = HEADER_PORTION_CLIENT_LEN - len(str(client_id))
-    logging.log(logging.INFO,"adding %d characters to client id %s" % (len_to_pad,client_id))
-    return str(client_id).encode('ascii') + ('\x00' * len_to_pad).encode('ascii')
-
-def pad_path(path):
-    len_to_pad = HEADER_PORTION_PATH_LEN - len(str(path))
-    logging.log(logging.INFO,"adding %d characters to path %s" % (len_to_pad,path))
-    return str(path).encode('ascii') + ('\x00' * len_to_pad).encode('ascii')
-
-def pad_size(size):
-    len_to_pad = HEADER_PORTION_SIZE_LEN - len(str(size))
-    logging.log(logging.INFO,"adding %d characters to size %s" % (len_to_pad,size))
-    return str(size).encode('ascii') + ('\x00' * len_to_pad).encode('ascii')
+            network_utils.ship_file_to_server(client_id,file_path,port=CONNECTION_PORT)
 
 def dump_file_info(path,size,encrypted_size):
     logging.log(logging.INFO,"==== SENDING FILE : INFO ====")
