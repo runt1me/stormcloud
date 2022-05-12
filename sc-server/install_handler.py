7354c623078c2ce1fdebbe4e7d50eed40befba0c
Dark Age Developers <rtchess.maniac@gmail.com>
2022-05-12 20:34:19 +0000

    server side json recv until eol

--- sc-server/install_handler.py
+++ sc-server/install_handler.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import socket, ssl
+import json
 
 BIND_PORT=8443
 
@@ -24,8 +25,9 @@ def main():
                     ssl_version=ssl.PROTOCOL_TLS
             )
     
-            request = wrappedSocket.recv(1024)
+            request = recv_json_until_eol(wrappedSocket)
             print(request)
+            print(type(request))
     
             if request:
                 response_data = b'RAT'
@@ -35,7 +37,31 @@ def main():
     finally:
         wrappedSocket.close()
 
+def recv_json_until_eol(socket):
+    # Borrowed from https://github.com/mdebbar/jsonsocket
 
+    # read the length of the data, letter by letter until we reach EOL
+    length_bytes = bytearray()
+    char = socket.recv(1)
+    while char != bytes('\n',encoding="UTF-8"):
+      length_bytes += char
+
+      char = socket.recv(1)
+    total = int(length_bytes)
+
+    # use a memoryview to receive the data chunk by chunk efficiently
+    view = memoryview(bytearray(total))
+    next_offset = 0
+    while total - next_offset > 0:
+      recv_size = socket.recv_into(view[next_offset:], total - next_offset)
+      next_offset += recv_size
+
+    try:
+      deserialized = json.loads(view.tobytes())
+    except (TypeError, ValueError) as e:
+      raise Exception('Data received was not in JSON format')
+
+    return deserialized
 
 if __name__ == "__main__":
     main()
