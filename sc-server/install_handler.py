11dfb023a39f628f8ee2557803e1e82e3f985e2d
Dark Age Developers <rtchess.maniac@gmail.com>
2022-08-10 20:08:15 +0000

    refactor install handler

--- sc-server/install_handler.py
+++ sc-server/install_handler.py
@@ -2,41 +2,26 @@
 import socket, ssl
 import json
 from datetime import datetime
+import argparse
 
+import logging
+
+import network_utils as scnet
 import database_utils as db
 import crypto_utils
+import install_utils
 
-BIND_PORT=8443
-
-def main():
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-    context.load_cert_chain(certfile="/root/certs/cert.pem")
-
-    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.bind(('0.0.0.0',BIND_PORT))
-    s.listen(5)
-
-    print("Listening for connections")
+def main(listen_port):
+    install_utils.initialize_logging()
+    s = scnet.initialize_socket(listen_port=listen_port)
 
     try:
         while True:
-            # TODO: use actual signed cert for SSL
-            # and use TLS 1.3
-            connection, client_address = s.accept()
-            wrappedSocket = ssl.wrap_socket(
-                    connection,
-                    server_side=True,
-                    certfile="/root/certs/cert.pem",
-                    keyfile="/root/certs/cert.pem",
-                    ssl_version=ssl.PROTOCOL_TLS
-            )
-    
-            request = recv_json_until_eol(wrappedSocket)
-    
+            wrappedSocket = scnet.accept_and_wrap_socket(s)
+            request       = scnet.recv_json_until_eol(wrappedSocket)
+
             if request:
               ret_code, response_data = handle_request(request)
-              # Send the length of the serialized data first, then send the data
-              # wrappedSocket.send(bytes('%d\n',encoding="utf-8") % len(response_data))
               wrappedSocket.sendall(bytes(response_data,encoding="utf-8"))
 
             else:
@@ -44,35 +29,8 @@ def main():
     finally:
         wrappedSocket.close()
 
-def recv_json_until_eol(socket):
-    # Borrowed from https://github.com/mdebbar/jsonsocket
-
-    # read the length of the data, letter by letter until we reach EOL
-    length_bytes = bytearray()
-    char = socket.recv(1)
-    while char != bytes('\n',encoding="UTF-8"):
-      length_bytes += char
-
-      char = socket.recv(1)
-    total = int(length_bytes)
-
-    # use a memoryview to receive the data chunk by chunk efficiently
-    view = memoryview(bytearray(total))
-    next_offset = 0
-    while total - next_offset > 0:
-      recv_size = socket.recv_into(view[next_offset:], total - next_offset)
-      next_offset += recv_size
-
-    try:
-      deserialized = json.loads(view.tobytes())
-    except (TypeError, ValueError) as e:
-      # TODO: Send error code back to client
-      raise Exception('Data received was not in JSON format')
-
-    return deserialized
-
 def handle_request(request):
-    print(request)
+    logging.log(logging.INFO,request)
 
     if 'request_type' not in request.keys():
         return -1,json.dumps({'response':'Bad request.'})
@@ -88,7 +46,7 @@ def handle_request(request):
     return ret_code, response_data
 
 def handle_hello_request(request):
-    print("Server handling hello request.")
+    logging.log(logging.INFO,"Server handling hello request.")
     response_data = json.dumps({
         'hello-response': 'Goodbye'
     })
@@ -96,11 +54,11 @@ def handle_hello_request(request):
     return 0, response_data
 
 def handle_register_new_device_request(request):
-    print("Server handling new device request.")
+    logging.log(logging.INFO,"Server handling new device request.")
     customer_id      = db.get_customer_id_by_api_key(request['api_key'])
 
     if not customer_id:
-        print("Could not find customer ID for the given API key: %s" % request['api_key'])
+        logging.log(logging.INFO,"Could not find customer ID for the given API key: %s" % request['api_key'])
 
     device_name      = request['device_name']
     ip_address       = request['ip_address']
@@ -109,13 +67,6 @@ def handle_register_new_device_request(request):
     device_status    = request['device_status']
 
     # TODO: sanitize all strings for SQL injection
-
-    # Verify that the device is not already in the database
-    # If it is not, create a new device in the device table
-    # For now, a device is considered uniquely identified by its customer id, hostname and ip address
-    ret = verify_device_uniqueness(customer_id,device_name,ip_address)
-    if ret != 0:
-        print("Device is already in database, device id: %s" % ret)
 
     # TODO: probably separate all of the above code into a "validate_request" or some similar type of function
     last_callback = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -135,9 +86,9 @@ def handle_register_new_device_request(request):
 
     return 0, response_data
 
-def verify_device_uniqueness(customer_id,device_name,ip_address):
-    # TODO: probably a database-side pass the info and database returns <device id> or 0
-    return 0
-
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", default=8443, type=int, help="port to listen on for install handling")
+    args = parser.parse_args()
+
+    main(args.port)
