c8d9b6224a450fcf5184812c90cd979af62634a3
Dark Age Developers <rtchess.maniac@gmail.com>
2022-09-10 23:32:34 +0000

    server side trying to get versioning working

--- sc-server/backup_utils.py
+++ sc-server/backup_utils.py
@@ -1,15 +1,16 @@
 import pathlib
 import logging
 import os
+import glob
 
 import crypto_utils
 
-def store_file(customer_id,device_id,path_to_device_secret_key,file_path,file_raw_content):
+def store_file(customer_id,device_id,path_to_device_secret_key,file_path,file_raw_content,max_versions):
     decrypted_raw_content, _ = crypto_utils.decrypt_msg(path_to_device_secret_key,file_raw_content,decode=False)
     decrypted_path, _        = crypto_utils.decrypt_msg(path_to_device_secret_key,file_path,decode=True)
 
     path_on_server, device_root_directory_on_server = get_server_path(customer_id,device_id,decrypted_path)
-    write_file_to_disk(path_on_server,decrypted_raw_content)
+    write_file_to_disk(path_on_server,decrypted_raw_content,max_versions)
 
     log_file_info(decrypted_path,device_id,path_on_server)
 
@@ -43,12 +44,59 @@ def get_server_path(customer_id,device_id,decrypted_path):
     path = path.replace("//","/")
     return path, device_root_directory_on_server
 
-def write_file_to_disk(path,content):
-    # TODO: Check if file exists
+def write_file_to_disk(path,content,max_versions):
+    if os.path.exists(path):
+        # Find highest version of file
+        print("Looking for highest version of file")
+        print("Maximum number of versions stored: %d" % max_versions)
 
-    # If it does exist, mv original file to rev2 -> move rev2 -> rev3, etc.
-    # Find max number of revs and roll off last one
-    # make a hidden directory .SCREV/ORIGINAL_NAME.ext.SCREV2
+        sc_version_directory = os.path.dirname(path) + "/.SCVERS/"
+
+        print("Looking for versions at path: %s" % sc_version_directory)
+        os.makedirs(os.path.dirname(sc_version_directory), exist_ok=True)
+
+        # List all files in path that match *filename* = all versions of the file
+        file_name = get_file_name(path)
+        print("Checking in %s for matches *%s*" % (sc_version_directory,file_name))
+        match = glob.glob(sc_version_directory+"*%s*" % file_name)
+
+        print(match)
+        print(type(match))
+
+        if not match:
+            print("no matches found, creating ver2")
+            # No previous versions exist, copy original path to VER2
+            old_version_full_path = sc_version_directory + file_name + ".SCVER2"
+            os.rename(path,old_version_full_path)
+
+        else:
+            print("Found matches:" %match)
+            match.sort().reverse()
+            # sort list to descending order (5,4,3,2)
+            # move 5 -> 6
+            # move 4 -> 5
+            # move 3 -> 4
+            # move 2 -> 3
+            # move current -> 2
+            for m in match:
+                file_name = get_file_name(m)
+                print("Looking at filename: %s" %file_name)
+
+                # get the thing from after the last occurrence of .SCVER
+                version = int(file_name.split(".SCVER")[-1])
+                next_version = version + 1
+
+                string_to_replace   = ".SCVER%d"%version
+                replacement_string  = ".SCVER%d"%next_version
+
+                print("Found version: %d" % version)
+                print("== Renaming ==")
+                print(match)
+                print(match.replace(string_to_replace,replacement_string))
+
+                os.rename(match,match.replace(string_to_replace,replacement_string))
+
+    # Finally, write the original content at the originally specified file path
     os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, "wb") as outfile:
         outfile.write(content)
