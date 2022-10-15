6e43a5c3e226b2722a0a4130cd3ba283b21d6415
Ryan Thomas <rtchess.maniac@gmail.com>
2022-10-15 12:05:14 -0600

    fixes logs being sent every other run

--- sc-client/logging_utils.py
+++ sc-client/logging_utils.py
@@ -12,25 +12,22 @@ import crypto_utils
 import network_utils
 
 def send_logs_to_server(api_key,agent_id):
-    #TODO: fix issue with logs not getting written if a logfile already exists at program startup
     logfiles_list = get_logfiles(uuid=agent_id)
     for logfile in logfiles_list:
-        print("Shipping logfile: %s" % logfile)
         filepath = pathlib.Path(logfile)    
         network_utils.ship_file_to_server(api_key,agent_id,filepath)
 
-        print("Removing logfile: %s" % logfile)
         os.remove(logfile)
 
 def get_logfiles(uuid):
     return glob.glob("%s*.log" % uuid)
 
 def initialize_logging(uuid):
-    print("Creating new logfile.")
     logging.basicConfig(
         filename='%s_%s.log' % (uuid,datetime.now().strftime("%Y-%m-%d")),
         filemode='a',
         format='%(asctime)s %(levelname)-8s %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
-        level=logging.DEBUG
+        level=logging.DEBUG,
+        force=True
     )
