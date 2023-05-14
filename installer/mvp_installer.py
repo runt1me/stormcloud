7f993030934f70ccab02a3e7f7db529e61ef3f0d
Ryan Thomas <rtchess.maniac@gmail.com>
2023-05-14 18:10:38 -0400

    big dump of installer changes

--- installer/mvp_installer.py
+++ installer/mvp_installer.py
@@ -6,41 +6,24 @@ import platform
 import requests
 import psutil
 import netifaces
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QPushButton, QProgressBar, QWizard, QWizardPage, QLineEdit, QTextEdit, QMessageBox, QFormLayout, QCheckBox, QFileDialog
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QPushButton, QProgressBar
+from PyQt5.QtWidgets import QWizard, QWizardPage, QLineEdit, QTextEdit, QMessageBox, QFormLayout
 from PyQt5.QtWidgets import QCheckBox, QFileDialog, QScrollArea, QWidget, QGridLayout
-
-def download_to_folder(url, folder, file_name):
-    try:
-        response = requests.get(url)
-    except SSLError as e:
-        logging.log(logging.ERROR, "Caught SSL Error when trying to download from %s: %s" % (url,e))
-    except Exception as e:
-        logging.log(logging.ERROR, "Caught exception when trying to download from %s: %s" % (url,e))
-        
-    if response:
-        try:
-            full_path = folder + file_name
-            os.makedirs(os.path.dirname(full_path), exist_ok=True)
-            
-            with open(full_path,'wb') as f:
-                for chunk in response.iter_content():
-                    f.write(chunk)
-            
-            # Pass case
-            return (0, full_path)
-        except Exception as e:
-            logging.log(logging.ERROR, "Caught exception when trying to write stormcloud to file: %s. Error: %s" % (full_path,e))
-
-    # Fail case
-    return (1, None)
+from PyQt5.QtCore import Qt
 
 class Installer(QWizard):
     def __init__(self):
         super().__init__()
+        # TODO: logging
 
         self.setWindowTitle("Stormcloud Installer")
         self.setFixedSize(640, 480)
+
+        # Scoping these variables to the installer so that I can use them later
+        self.system_info   = None
+        self.api_key       = None
+        self.target_folder = None
+
 
         self.addPage(WelcomePage())
         self.addPage(APIKeyPage())
@@ -77,7 +60,6 @@ class APIKeyPage(QWizardPage):
             return False
 
     def validate_api_key(self, api_key):
-        # Replace this URL with your actual API URL for validation
         url = "https://www2.darkage.io:8443/api/validate-api-key"
         headers = {"Content-Type": "application/json"}
         data = {"api_key": api_key}        
@@ -89,6 +71,7 @@ class APIKeyPage(QWizardPage):
                 return True
             else:
                 return False
+
         except Exception as e:
             print(e)
             return False
@@ -97,26 +80,22 @@ class SystemInfoPage(QWizardPage):
     def __init__(self):
         super().__init__()
         self.setTitle("System Information")
-        self.system_info = self.get_system_info()
+        self.wizard().system_info = self.get_system_info()
 
         layout = QFormLayout()
-        for key, value in self.system_info.items():
+        for key, value in self.wizard().system_info.items():
             layout.addRow(QLabel(key), self.createReadOnlyText(str(value)))
         self.setLayout(layout)
 
-    def initializePage(self):
-        self.system_info = self.get_system_info()
-        self.system_info_text.setPlainText(json.dumps(self.system_info, indent=2))
-
     def get_system_info(self):
+        BYTES_IN_A_GB = 1073741824
         system_info = {
             "hostname": socket.gethostname(),
             "ip_address": self.get_ipv4_address_associated_with_default_gateway(),
-            "available_ram": str(round(psutil.virtual_memory().available / 1073741824, 1)) + " GB",
-            "total_ram": str(round(psutil.virtual_memory().total / 1073741824, 1)) + " GB",
+            "available_ram": str(round(psutil.virtual_memory().available / BYTES_IN_A_GB, 1)) + " GB",
+            "total_ram": str(round(psutil.virtual_memory().total / BYTES_IN_A_GB, 1)) + " GB",
             "operating_system": platform.platform(),
             "device_name": "foo"
-            # Add any other diagnostic information here
         }
         return system_info
 
@@ -135,25 +114,7 @@ class SystemInfoPage(QWizardPage):
         self.system_info["request_type"] = "register_new_device"
         self.system_info["device_status"] = 1
         self.system_info["device_type"] = "bar"
-        result = self.register_new_device(self.system_info)
-        if result:
-            return True
-        else:
-            QMessageBox.warning(self, "Error", "Failed to register the new device. Please try again.")
-            return False
 
-    def register_new_device(self, data):
-        url = "https://www2.darkage.io:8443/api/register-new-device"
-        headers = {"Content-Type": "application/json"}
-
-        try:
-            response = requests.post(url, headers=headers, json=data)
-            if response.status_code == 200:
-                return True
-            else:
-                return False
-        except:
-            return False
     def createReadOnlyText(self, text):
         readOnlyText = QTextEdit()
         readOnlyText.setPlainText(text)
@@ -167,12 +128,6 @@ class SystemInfoPage(QWizardPage):
             }
         """)
         return readOnlyText
-
-SERVER_NAME="www2.darkage.io"
-SERVER_PORT=8443
-STORMCLOUD_VERSION="1.0.0"
-
-stormcloud_client_url = "https://%s/sc-dist/windows-x86_64-stormcloud-client-%s.exe" % (SERVER_NAME, STORMCLOUD_VERSION)
 
 class BackupPage(QWizardPage):
     def __init__(self):
@@ -225,10 +180,23 @@ class BackupPage(QWizardPage):
         return self.wizard().pageIds()[self.wizard().currentPageIndex() + 1]
 
 class InstallPage(QWizardPage):
-    # TODO: Write code to do stormcloud installer stuff
-    
     def __init__(self):
         super().__init__()
+
+        self.SERVER_NAME="www2.darkage.io"
+        self.SERVER_PORT_API=8443
+        self.SERVER_PORT_DOWNLOAD=443
+        self.STORMCLOUD_VERSION="1.0.0"
+
+        self.stormcloud_client_url   = "https://%s:%s/sc-dist/windows-x86_64-stormcloud-client-%s.exe" % (
+            self.SERVER_NAME,
+            self.SERVER_PORT_DOWNLOAD,
+            self.STORMCLOUD_VERSION
+        )
+        self.register_new_device_url   = "https://%s:%s/api/register-new-device" % (
+            self.SERVER_NAME,
+            self.SERVER_PORT_API
+        )
 
         layout = QVBoxLayout()
         label = QLabel("Installing Stormcloud...")
@@ -243,13 +211,131 @@ class InstallPage(QWizardPage):
         self.install()
 
     def install(self):
-        if not target_folder.endswith('\\'):
+        register_result = self.register_new_device(self.wizard().system_info)
+        download_result = self.download_to_folder(WINDOWS_OS_SC_CLIENT_URL, self.wizard().target_folder, "stormcloud.exe")
+        configure_result = self.configure_settings(
+            send_logs=1, backup_time=22, keepalive_freq=300,
+            backup_paths=self.wizard().system_info['backup_paths'],
+            backup_paths_recursive=self.wizard().system_info['backup_paths_recursive'],
+            secret_key=register_result['secret_key'],
+            agent_id=register_result['agent_id'],
+            api_key=self.wizard().system_info['api_key'],
+            target_folder=self.wizard().target_folder,
+        )
+
+        # TODO: configure_persistence()
+
+        if result:
+            return True
+        else:
+            QMessageBox.warning(self, "Error", "Failed to register the new device. Please try again.")
+            return False
+
+    def register_new_device(self, data):
+        headers = {"Content-Type": "application/json"}
+
+        try:
+            response = requests.post(url=self.register_new_device_url, headers=headers, json=data)
+            if response.status_code == 200:
+                return response.json
+            else:
+                return None
+        except:
+            return None
+
+    def configure_settings(send_logs, backup_time, keepalive_freq, backup_paths, backup_paths_recursive, secret_key, agent_id, api_key, target_folder, os_info):
+        backup_time        = int(backup_time)
+        keepalive_freq     = int(keepalive_freq)
+
+        if not target_folder.endswith("\\"):
             target_folder += "\\"
-        return download_to_folder(WINDOWS_OS_SC_CLIENT_URL, target_folder, "stormcloud.exe")
+
+        settings_file_path = target_folder + "settings.cfg"
+        os.makedirs(os.path.dirname(settings_file_path), exist_ok=True)
+        with open(settings_file_path, "w") as settings_file:
+            lines_to_write = []
+
+            # Logging
+            lines_to_write.append("# send logging and error information to dark age servers")
+            lines_to_write.append("# to assist with development/bug fixes/discovery of errors")
+            lines_to_write.append("# 1 = ON, 0 = OFF")
+            if send_logs:
+                lines_to_write.append("SEND_LOGS 1")
+            else:
+                lines_to_write.append("SEND_LOGS 0")
+
+            # Backup time
+            lines_to_write.append("# controls backup time of day")
+            lines_to_write.append("# hour of the day/24hr time")
+            lines_to_write.append("# i.e. 23 = 11PM (system time)")
+            lines_to_write.append("BACKUP_TIME %d" % backup_time)
+
+            # Keepalive frequency
+            lines_to_write.append("# controls how frequently this device will send keepalive message to the stormcloud servers.")
+            lines_to_write.append("# Interval in seconds (90=send keepalive every 90 seconds)")
+            lines_to_write.append("KEEPALIVE_FREQ %d" % keepalive_freq)
+
+            # Secret Key
+            lines_to_write.append("# symmetric key for device encryption")
+            lines_to_write.append("SECRET_KEY %s" % secret_key)
+            
+            # Agent ID
+            lines_to_write.append("# Agent ID, for identifying this device to the stormcloud servers")
+            lines_to_write.append("AGENT_ID %s" % agent_id)
+
+            # API key my husband is so smart
+            lines_to_write.append("# API key")
+            lines_to_write.append("API_KEY %s" % api_key)
+
+            # Backup paths
+            lines_to_write.append("# paths to backup")
+            lines_to_write.append("BACKUP_PATHS")
+            for bp in backup_paths:
+                lines_to_write.append("%s" % bp)
+
+            # Recursive backup paths
+            lines_to_write.append("# paths to recursively backup")
+            lines_to_write.append("RECURSIVE_BACKUP_PATHS")
+            if backup_paths_recursive:
+                for rbp in backup_paths_recursive:
+                    lines_to_write.append("%s" % rbp)
+
+            output_string = "\n".join(lines_to_write)
+            settings_file.write(output_string)
+
+    def download_to_folder(url, folder, file_name):
+        if not folder.endswith('\\'):
+            folder += "\\"
+
+        try:
+            response = requests.get(url)
+        except SSLError as e:
+            logging.log(logging.ERROR, "Caught SSL Error when trying to download from %s: %s" % (url,e))
+        except Exception as e:
+            logging.log(logging.ERROR, "Caught exception when trying to download from %s: %s" % (url,e))
+            
+        if response:
+            try:
+                full_path = folder + file_name
+                os.makedirs(os.path.dirname(full_path), exist_ok=True)
+                
+                with open(full_path,'wb') as f:
+                    for chunk in response.iter_content():
+                        f.write(chunk)
+                
+                # Pass case
+                return (0, full_path)
+            except Exception as e:
+                logging.log(logging.ERROR, "Caught exception when trying to write stormcloud to file: %s. Error: %s" % (full_path,e))
+
+        # Fail case
+        return (1, None)
 
 class FinishPage(QWizardPage):
     def __init__(self):
         super().__init__()
+
+        # TODO: have a checkbox to launch the installer
 
         layout = QVBoxLayout()
         label = QLabel("Installation completed! Click 'Finish' to close the installer.")
