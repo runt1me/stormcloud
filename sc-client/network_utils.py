72e2015cdaa910064a50034656acf9d4c48b364c
Ryan Thomas <rtchess.maniac@gmail.com>
2024-07-12 23:23:44 -0400

    removes second layer of encryption on client side network utils

--- sc-client/network_utils.py
+++ sc-client/network_utils.py
@@ -2,7 +2,9 @@ import json
 import requests
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
+import os
 import logging
+import base64
 
 import crypto_utils
 
@@ -19,35 +21,33 @@ THRESHOLD_MB = 200
 CHUNK_SIZE = ONE_MB
 
 def ship_file_to_server(api_key,agent_id,secret_key,path):
-    unencrypted_path_to_encrypted_file, size_of_encrypted_content    = crypto_utils.encrypt_file(path,secret_key)
-    encrypted_path, _ = crypto_utils.encrypt_content(path,secret_key)
+    #unencrypted_path_to_encrypted_file, size_of_encrypted_content    = crypto_utils.encrypt_file(path,secret_key)
+    #encrypted_path, _ = crypto_utils.encrypt_content(path,secret_key)
 
-    logging.log(logging.INFO,dump_file_info(path,size_of_encrypted_content))
+    size = os.path.getsize(path)
 
-    if size_of_encrypted_content > THRESHOLD_MB * ONE_MB:
+    logging.log(logging.INFO,dump_file_info(path,size))
+
+    if size > THRESHOLD_MB * ONE_MB:
         logging.log(logging.INFO, "File size over %dMB, using MultipartEncoder" % THRESHOLD_MB)
 
         ret = stream_upload_file(
             api_key,
             agent_id,
-            encrypted_path,
-            unencrypted_path_to_encrypted_file,
-            size_of_encrypted_content
+            path
         )
 
     else:
         ret = upload_file(
             api_key,
             agent_id,
-            encrypted_path,
-            unencrypted_path_to_encrypted_file,
-            size_of_encrypted_content
+            path
         )
 
-    crypto_utils.remove_temp_file(unencrypted_path_to_encrypted_file)
+    #crypto_utils.remove_temp_file(unencrypted_path_to_encrypted_file)
     return ret
 
-def stream_upload_file(api_key,agent_id,encrypted_path,unencrypted_path_to_encrypted_file,size_of_encrypted_content):
+def stream_upload_file(api_key,agent_id,local_file_path):
     url = API_ENDPOINT_BACKUP_FILE_STREAM
     response = None
 
@@ -55,10 +55,12 @@ def stream_upload_file(api_key,agent_id,encrypted_path,unencrypted_path_to_encry
         'request_type': "backup_file",
         'api_key': api_key,
         'agent_id': agent_id,
-        'file_path': encrypted_path.decode("UTF-8"),
+
+        # WindowsPath obj -> str -> encode UTF-8 to convert to bytes -> base64 encode -> utf-8 decode -> serialize as JSON
+        'file_path': base64.b64encode(str(local_file_path).encode("utf-8")).decode('utf-8'),
 
         # must provide 'filename' parameter in order for flask to properly interpret this as a file
-        'file_content': ('filename', open(unencrypted_path_to_encrypted_file, 'rb'), 'application/octet-stream')
+        'file_content': ('filename', open(local_file_path, 'rb'), 'application/octet-stream')
     }
 
     enc = MultipartEncoder(fields=fields_dict)
@@ -70,7 +72,7 @@ def stream_upload_file(api_key,agent_id,encrypted_path,unencrypted_path_to_encry
     finally:
         return response.status_code if response else 500
 
-def upload_file(api_key,agent_id,encrypted_path,unencrypted_path_to_encrypted_file,size_of_encrypted_content):
+def upload_file(api_key,agent_id,local_file_path):
     url = API_ENDPOINT_BACKUP_FILE
     response = None
 
@@ -78,17 +80,19 @@ def upload_file(api_key,agent_id,encrypted_path,unencrypted_path_to_encrypted_fi
         'request_type': "backup_file",
         'api_key': api_key,
         'agent_id': agent_id,
-        'file_path': encrypted_path.decode("UTF-8")
+
+        # WindowsPath obj -> str -> encode UTF-8 to convert to bytes -> base64 encode -> utf-8 decode -> serialize as JSON
+        'file_path': base64.b64encode(str(local_file_path).encode("utf-8")).decode('utf-8')
     })
 
     # Since we're not streaming here, read the whole file into memory before sending.
-    encrypted_content = open(unencrypted_path_to_encrypted_file, 'rb').read()
+    content = open(local_file_path, 'rb').read()
 
     # Including JSON object as part of "files" field
     # Because I cannot include both separately in a single multipart/form-data request.
     # See https://stackoverflow.com/questions/35939761/how-to-send-json-as-part-of-multipart-post-request
     files = {
-        'file_content': encrypted_content,
+        'file_content': content,
         'json': (None, json_data, 'application/json')
     }
 
@@ -149,7 +153,7 @@ def tls_send_json_data_get(json_data_as_string, expected_response_code, show_jso
         else:
             return (1, None)
 
-def dump_file_info(path,size_of_encrypted_content):
-    logging.log(logging.INFO,"==== SENDING FILE : INFO ====")
+def dump_file_info(path,size):
+    logging.log(logging.INFO,"== SENDING FILE : ==")
     logging.log(logging.INFO,"\tPATH: %s" %path)
-    logging.log(logging.INFO,"\tSIZE WHEN ENCRYPTED: %d" %size_of_encrypted_content)
+    logging.log(logging.INFO,"\tSIZE: %d" %size)
