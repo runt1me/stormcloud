8387d3ff01bd282d6c8c4a98db9ec3686ffcfaeb
Dark Age Developers <rtchess.maniac@gmail.com>
2025-01-21 01:56:11 +0000

    adding anthropic credentials environment file

--- sc-server/wsgi.py
+++ sc-server/wsgi.py
@@ -1,3 +1,12 @@
 import sys
 sys.path.insert(0,"/var/www/api/")
+
+import os
+import dotenv
+
+# Load environment variables
+env_path = '/etc/stormcloud/env'
+if os.path.exists(env_path):
+    dotenv.load_dotenv(env_path)
+
 from server_main import app as application
